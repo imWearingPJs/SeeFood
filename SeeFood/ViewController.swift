a0699ddb520984792b08a1d23e3b4bcc1456885e
imWearingPJs <michaelkozub@gmail.com>
2019-02-21 12:32:03 -0700

    CoreML to detect butterfly in results

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -27,9 +27,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Can't convert the UIImage to a CIImage")
+            }
+            detect(image: ciimage)
+            
         }
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect (image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreMLModel failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print("kozub result")
+            print(results)
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("butterfly") {
+                    self.navigationItem.title = "Butterfly!"
+                } else {
+                    self.navigationItem.title = "Not Butterfly!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print("kozub error")
+            print(error)
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
